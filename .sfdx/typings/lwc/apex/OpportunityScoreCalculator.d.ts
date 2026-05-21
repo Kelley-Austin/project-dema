fd91acddd06e1c1375c0d8b628bbf5706cd8bd9c
claresegrue-prft <claresegrue-prft@users.noreply.github.com>
2026-05-21 09:38:35 -0500

    wip: save before branch switch

---
+++ .sfdx/typings/lwc/apex/OpportunityScoreCalculator.d.ts
@@ -0,0 +1,6 @@
+declare module "@salesforce/apex/OpportunityScoreCalculator.recalculateScore" {
+  export default function recalculateScore(param: {opportunityId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/OpportunityScoreCalculator.getScoreData" {
+  export default function getScoreData(param: {opportunityId: any}): Promise<any>;
+}
